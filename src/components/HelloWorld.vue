dfb790784fb2c3b8c5e2bcff8d45f12cb6d7452b
Adwin Ying <adwinying@gmail.com>
2021-01-13 01:38:30 +0900

    Fix lint errors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,33 +2,38 @@
   <h1>{{ msg }}</h1>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank">Vite Documentation</a> |
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank">Vue 3 Documentation</a>
   </p>
 
   <p>
     Recommended setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank">VSCode</a>
     +
     <a
       href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >Vetur</a>
+      target="_blank">Vetur</a>
     +
     <a
       href="https://marketplace.visualstudio.com/items?itemName=znck.vue-language-features"
-      target="_blank"
-    >Vue DX</a>
+      target="_blank">Vue DX</a>
   </p>
   <p>
     Make sure to use workspace version of TypeScript to get improved support via
     <a
       href="https://github.com/znck/vue-developer-experience"
-      target="_blank"
-    >@vuedx</a>.
-    <br />Note @vuedx is still experimental and this setup is provided for early feedback.
+      target="_blank">@vuedx</a>.
+    <br>Note @vuedx is still experimental and this setup is provided for early feedback.
   </p>
-  <button @click="count++">count is: {{ count }}</button>
+  <button @click="count++">
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -36,21 +41,21 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    const count = ref(0);
+    return { count };
+  },
+});
 </script>
 
 <style scoped>
